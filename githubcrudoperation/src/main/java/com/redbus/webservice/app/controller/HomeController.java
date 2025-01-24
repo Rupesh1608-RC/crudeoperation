151644aebdc754317c45cbb8e471e580ab90560e
Shree <Shree@DESKTOP-9SF6INJ>
2025-01-24 11:17:52 +0530

    add methods

---
+++ githubcrudoperation/src/main/java/com/redbus/webservice/app/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.redbus.webservice.app.controller;
+
+public class HomeController {
+	
+	public void m1() {
+		
+	}
+	
+	public void m2() {
+		
+		
+	}
+	
+	public void m3() {
+		
+	}
+
+}
